5dea7c45b19334118a9cc5dde77601acd742439d
nozomi0712 <s.mo.19930712@gmail.com>
2020-04-25 11:18:07 +0900

    add_chat_functiom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @group = Group.find(params[:id])
+    @post = Post.new
+    @posts = @group.posts.includes(:user)
+
+  end
+  
+  def create
+    @post = @group.posts.create(post_params)
+    if @post.save
+      redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
+    else
+      @posts = @group.posts.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+  private 
+  def set_group 
+    @group = Group.find(params[:group_id])
+  end
+  def post_params
+    params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
